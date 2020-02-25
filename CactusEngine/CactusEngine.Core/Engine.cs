e2f14e800b8d0c9f1f16f59194e0ee2670866249
Mathieu Charron <mathieucharron42@gmail.com>
2020-02-24 23:04:47 -0500

    Inital implementation of sfml

---
+++ CactusEngine/CactusEngine.Core/Engine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CactusEngine.Core
+{
+    class Engine
+    {
+		public void SetupTask<TaskType>()
+			where TaskType : ITask, new()
+		{
+			TaskType task = new TaskType();
+			_task.Add(task);
+		}
+
+		private List<ITask> _task;
+
+		private void Tick()
+		{
+			foreach(ITask task in _task)
+			{
+				task.Execute(this);
+			}
+		}
+	}
+}
---
+++ CactusEngine/CactusEngine.Core/ITask.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CactusEngine.Core
+{
+    interface ITask
+    {
+        void Execute(Engine engine);
+    }
+}
